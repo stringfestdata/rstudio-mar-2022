1c736e2981bed351754b472dbf8dd9c4269acfd2
George Mount <gxm204@case.edu>
2022-02-20 13:49:30 -0500

    Update install.r

--- install.r
+++ install.r
@@ -2,5 +2,6 @@ install.packages('Lahman') # Our source data
 install.packages('tidyverse')  # "Metapackage," don't leave home without it!
 install.packages('psych')  # Descriptive stats
 install.packages('skimr')  # Data profiling
-install.packages('writexl') # Write to Excel -- data dump
-install.packages('openxlsx') # Write to Excel -- plus 
+install.packages('writexl') # Write to Excel # data dump
+install.packages('openxlsx') # Write to Excel # plus
+# https://github.com/jupyterhub/repo2docker/issues/237
