c3deaf8942f0d3a9e793d49ffc24e4f50a6881a0
George Mount <gxm204@case.edu>
2022-02-20 08:30:26 -0500

    Update install.r

--- install.r
+++ install.r
@@ -1,1 +1,6 @@
-install.packages("tidyverse")
+install.packages('Lahman') # Our source data
+install.packages('tidyverse')  # "Metapackage," don't leave home without it!
+install.packages('psych')  # Descriptive stats
+install.packages('skimr')  # Data profiling
+install.packages('writexl') # Write to Excel -- data dump
+install.packages('openxlsx') # Write to Excel -- plus 
